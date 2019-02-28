44117d15f5f355976017cd1d3c8e6f072ab03c65
lvino <553269233@qq.com>
2019-02-28 19:18:05 +1100

    'change'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>GraphQL Practise Site</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
